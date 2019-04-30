6fdb2352ae839bcf25731bb63eeb12ba309d7ac7
bhargavbunny <bhargavreddy1994u@gmail.com>
2019-04-29 22:59:33 -0400

    sleep time page is udated with code

--- src/views/sleep_time.vue
+++ src/views/sleep_time.vue
@@ -1,59 +1,15 @@
 <template>
-  <div class="row">
-    <div class="col-lg-6">
-      <div class="card">
-        <div class="card-header text-white bg-dark">
-          <ul class="nav nav-pills card-header-pills">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/sleep_time">My sleep time</router-link>
-          </li>
-        </ul>
-        </div>
-        <div class="container">
-            <form @submit.prevent="submit">
-              <br>
-
-              <div class="form-group row">
-                <label for="Date" class="col-3 col-form-label">Date</label>
-                <div class="col-9">
-                <input type="date" v-model="data.date"
-                    class="form-control"
-                    name="date"
-                    id="date"
-                    aria-describedby="helpDate"
-                    placeholder="Date"
-                    required>
-                  <small id="helpDate" class="form-text text-muted">Add the date for your sleep time.</small>
-                </div>
-                </div>
-
-            <div class="form-group row">
-          <label for="daily_foods" class="col-3 col-form-label">Food Items</label>
-          <div class="col-9">
-          <input type="text" v-model="data.sleep_time"
-                    class="form-control"
-                    name="daily_foods"
-                    id="daily_foods"
-                    aria-describedby="helpdaily_foods"
-                    placeholder="daily_foods"
-                    required>
-                  <small id="helpdaily_foods" class="form-text text-muted">Night or day</small>
-          </div>
-          </div>
-          <br>
-
-         <div class="form-group row">
-          <div class="offset-3 col-9">
-          <button type="submit" class="btn btn-primary">Submit</button>
-          </div>
-          </div>
-          </form>
-          </div>
-          </div>
-          </div>
-          </div>
-          
-
+  <form>
+  <div class="form-group">
+    <label for="date">enter date</label>
+    <input type="date" class="form-control" id="date" aria-describedby="dateHelp" placeholder="Enter date">
+  </div>
+  <div class="form-group">
+    <label for="type">day time sleep or night time</label>
+    <input type="text" class="form-control" id="day/night" placeholder="sleep time">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>     
 </template>
 
 <script>
--- src/views/updateprofile.vue
+++ src/views/updateprofile.vue
@@ -3,19 +3,19 @@
   <div class="form-group">
     <label for="exampleInputEmail1">Name</label>
     <input type="email" class="form-control" id="Name" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">enter your name</small>
+    <small id="text" class="form-text text-muted">enter your name</small>
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Birthday</label>
-    <input type="password" class="form-control" id="Birthday" placeholder="Password">
+    <input type="date" class="form-control" id="Birthday" placeholder="Password">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">Weight</label>
-    <input type="password" class="form-control" id="Weight" placeholder="Password">
+    <input type="text" class="form-control" id="Weight" placeholder="Password">
   </div>
   <div class="form-group">
     <label for="exampleInputPassword1">height</label>
-    <input type="password" class="form-control" id="Height" placeholder="Password">
+    <input type="text" class="form-control" id="Height" placeholder="Password">
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
